23b465dc3bf582f6719a9f3cc9d086bf70ebe21c
Nikll1010 <294758896@qq.com>
2018-12-08 20:58:04 +0800

    1111

--- src/lession2/HandleTest.java
+++ src/lession2/HandleTest.java
@@ -7,6 +7,7 @@
 package lession2;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -31,6 +32,9 @@ public class HandleTest{
         Thread.sleep(Time);
     }
 
+    public void testpass(){
+        System.out.println("test pass !!!");
+    }
     /**
      * 句柄id输出测试
      */
@@ -56,26 +60,47 @@ public class HandleTest{
 //        WebDriver driver = new FirefoxDriver();
         String URL = "https://www.jd.com/";
         this.openBrowser(driver,URL);
+        String handle1 = driver.getWindowHandle();
+        System.out.println(handle1);
         sleepTime();
         WebElement phone_link = driver.findElement(By.linkText("手机"));
         phone_link.click();
         sleepTime();
         String handle = driver.getWindowHandle();
+
+        //切换到新打开页面的句柄
         for (String temhandle:driver.getWindowHandles()){
-            if (!handle.equals(driver.getWindowHandles())){
-                driver.quit();
+            if (!temhandle.equals(handle1)){
+//                driver.quit();
                 driver.switchTo().window(temhandle);
             }
         }
-
+        String handle2 = driver.getWindowHandle();
+        System.out.println(handle2);
 
 
 
     }
 
+    public void test3() throws InterruptedException {
+        //利用Javascript处理竖向滚动条
+        String URL = "https://blog.csdn.net/";
+        this.openBrowser(driver,URL);
+        sleepTime();
+        //定义一个元素
+        WebElement element = driver.findElement(By.xpath("//div[@class='right_box footer_box csdn-tracking-statistics']/h3[@class = 'feed_new_tit']/span[@class='txt']"));
+        //创建一个执行实例
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        sleepTime();
+        //执行定位到该元素   arguments[0] 是js中的第一个参数
+        js.executeScript("arguments[0].scrollIntoView(true);",element);
+        testpass();
+    }
+
     public static void main(String[] args) throws InterruptedException {
         HandleTest handleTest = new HandleTest();
-
-        handleTest.test2();
+//        handleTest.test1();
+//        handleTest.test2();
+        handleTest.test3();
     }
 }
